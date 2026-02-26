09b7a36089b2456bc5c12dcfa14371b7bcfff2b5
Rob McKenna <14824673+rob-mckenna@users.noreply.github.com>
2026-02-26 07:02:50 +0000

    update main.tf

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1282,7 +1282,7 @@ resource "azurerm_windows_virtual_machine" "jumpbox" {
 
   os_disk {
     caching              = "ReadWrite"
-    storage_account_type = "Premium_LRS"
+    storage_account_type = "Standard_LRS"
   }
 
   source_image_reference {
